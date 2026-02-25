f1ca9080c02abbc725d4805206f6ed611a2ed055
Tarun <tarunmalviya8827@gmail.com>
2026-02-25 14:55:16 +0530

    update env files

--- config/Cloudinary.js
+++ config/Cloudinary.js
@@ -7,7 +7,7 @@ Cloudinary.config(
     {
         cloud_name: `${process.env.Cloudinary_CLOUD_NAME}`,
         api_key: `${process.env.Cloudinary_API_KEY}`,
-        api_secret: `${process.env.fOp9IIHTd9QuUj - JRE0OPimt8wg}`
+        api_secret: `${process.env.Cloudinary_API_SECRET}`
     })
 
 export default Cloudinary;
